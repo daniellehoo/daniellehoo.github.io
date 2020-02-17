6f05250804dcc50ff572be1fddd0d97e0ec8e888
Danielle Hoo <daniellehoo@Danielles-MacBook-Pro.local>
2020-02-17 15:11:47 -0500

    back at it

---
+++ src/components/landing.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function landing () {
+  return (
+  <div className="landing">
+    <h1>Danielle Hoo<span class="blinking-cursor">|</span></h1>
+  </div>
+  )
+}
+
+export default landing
---
+++ src/components/nav.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+function nav () {
+  return (
+  <div className="nav">
+    <ul>
+      <li>Work</li>
+      <li>Blog</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </div>
+  )
+}
+
+export default nav
