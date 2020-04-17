b7d1156e24eae93dcad012fffcd3c1bf15a24b95
Danielle Hoo <daniellem.hoo@gmail.com>
2020-04-17 17:42:19 -0400

    working on deloyment issues in github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import Project from "./components/project";
+import React, { useState } from 'react'
+import Project from './components/project'
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,67 +7,68 @@ import {
   Redirect,
   Link,
   useParams
-} from "react-router-dom";
+} from 'react-router-dom'
 
-import About from "./components/about";
-import Landing from "./components/landing";
-import Projects from "./components/projects";
-import Model from "./components/threejs";
+import About from './components/about'
+import Landing from './components/landing'
+import Projects from './components/projects'
+import Model from './components/threejs'
 
-import "./App.css";
+import './App.css'
 
 const projects = [
   {
     id: 0,
-    title: "Chinatown USA",
-    url: "chinatown_usa",
-    img: "https://live.staticflickr.com/65535/49593430952_712dc0cce1_b.jpg",
-    description: "Chinatown, USA was inspired by The Chinese in America"
+    title: 'Chinatown USA',
+    url: 'chinatown_usa',
+    img: 'https://live.staticflickr.com/65535/49593430952_712dc0cce1_b.jpg',
+    description: 'Chinatown, USA was inspired by The Chinese in America'
   },
   {
     id: 1,
     title: "Our Children's Trust",
-    url: "our_childrens_trust",
-    img: "https://live.staticflickr.com/65535/49592718198_6760f8c00d_b.jpg",
-    
+    url: 'our_childrens_trust',
+    img: 'https://live.staticflickr.com/65535/49592718198_6760f8c00d_b.jpg',
+
     description: "our childreen's trustdfssdf "
   },
   {
     id: 2,
-    title: "Nicaragua",
-    url: "nicaragua",
-    img: "https://live.staticflickr.com/65535/49593769191_4de582e8d0_b.jpg",
-    description: "GIEI NICA "
+    title: 'Nicaragua',
+    url: 'nicaragua',
+    img: 'https://live.staticflickr.com/65535/49593769191_4de582e8d0_b.jpg',
+    description: 'GIEI NICA '
   },
 
   {
     id: 3,
-    title: "Amnesty International",
-    url: "amnesty_international",
-    img: "https://live.staticflickr.com/65535/49593280613_513f9fe243_b.jpg",
-    description: "teear gassss "
+    title: 'Amnesty International',
+    url: 'amnesty_international',
+    img: 'https://live.staticflickr.com/65535/49593280613_513f9fe243_b.jpg',
+    description: 'teear gassss '
   }
-];
+]
 
-const ProjectContext = React.createContext(projects);
+const ProjectContext = React.createContext(projects)
 
-function App() {
+function App () {
   return (
-      <div className="App">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route path="/about" component={About} />
-            <Route path="/projects"
-            render={(props) => <Projects {...props} projectsData={projects} />} />
-            <Route path="/:url">
-              <Project 
-              data={projects}/>
-            </Route>
-          </Switch>
-        </Router>
-      </div>
-  );
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route path='/:url'>
+            <Project data={projects} />
+          <Route
+            path='/projects'
+            render={props => <Projects {...props} projectsData={projects} />}
+          />
+            <Route path='/about' component={About} />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App
