26799c338e7da294d20046363006ff8a9fdfabdd
Danielle Hoo <daniellem.hoo@gmail.com>
2020-04-23 14:32:36 -0400

    updated history for projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -7,11 +7,11 @@ import fish from "../assets/blue_fish.png";
 function projects(props) {
   const [id, setId] = useState("/");
   const projectsData = props.projectsData;
+  // const history = useHistory();
 
   const grid = projectsData.map(project => (
     <div className={`project id_${project.id}`} key={project.id}>
-      {/* <a href={`/${project.url}`}> */}
-        <a href={`/projects/${project.url}`} onClick={() => handleClick(project)}>
+        <a href={`/projects/${project.url}`} onClick={() => handleClick(project.url)}>
 
         {project.title}
       </a>
@@ -20,7 +20,7 @@ function projects(props) {
   ));
 
   function handleClick(project){
-    console.log('click')
+    this.props.history.push(`/projects/${project.url}`)
   }
 
   return (
