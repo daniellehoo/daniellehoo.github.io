8cc27b95e13859d79e7771b00df7a74d6c921a9f
Danielle Hoo <daniellem.hoo@gmail.com>
2020-04-23 14:38:24 -0400

    Updates

---
+++ precache-manifest.1733c6757196da3ed5f463d43fd58e0e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3067c467e73241960d33",
+    "url": "daniellehoo.com/static/css/main.a8b3c280.chunk.css"
+  },
+  {
+    "revision": "3067c467e73241960d33",
+    "url": "daniellehoo.com/static/js/main.9066feb0.chunk.js"
+  },
+  {
+    "revision": "949e537e3fdac02ff68d",
+    "url": "daniellehoo.com/static/js/runtime~main.0e3555c5.js"
+  },
+  {
+    "revision": "cad3609e8bf967a4a96e",
+    "url": "daniellehoo.com/static/js/2.78b70f27.chunk.js"
+  },
+  {
+    "revision": "a499faa031847a5bcd56ba44876f1809",
+    "url": "daniellehoo.com/static/media/cat_icon.a499faa0.png"
+  },
+  {
+    "revision": "d47132d1715262c02402f923a5acbbd9",
+    "url": "daniellehoo.com/static/media/soy_sauce.d47132d1.png"
+  },
+  {
+    "revision": "eb9ff440db7d555755b73932d774bec0",
+    "url": "daniellehoo.com/static/media/luckycat.eb9ff440.glb"
+  },
+  {
+    "revision": "d24dc50d7fd4d7f96ab14d6b1b75eac3",
+    "url": "daniellehoo.com/static/media/blue_fish.d24dc50d.png"
+  },
+  {
+    "revision": "58a658a26c674d684cf336e00d1861f5",
+    "url": "daniellehoo.com/index.html"
+  }
+];
