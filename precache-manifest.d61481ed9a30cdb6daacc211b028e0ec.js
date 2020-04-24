151ffb15e3084a6e464980f6c98d00deff429501
Danielle Hoo <daniellem.hoo@gmail.com>
2020-04-24 15:19:08 -0400

    Updates

---
+++ precache-manifest.d61481ed9a30cdb6daacc211b028e0ec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d34b6a48307072f7f0ad",
+    "url": "/static/css/main.282f0a18.chunk.css"
+  },
+  {
+    "revision": "d34b6a48307072f7f0ad",
+    "url": "/static/js/main.87ac3106.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cad3609e8bf967a4a96e",
+    "url": "/static/js/2.78b70f27.chunk.js"
+  },
+  {
+    "revision": "a499faa031847a5bcd56ba44876f1809",
+    "url": "/static/media/cat_icon.a499faa0.png"
+  },
+  {
+    "revision": "d47132d1715262c02402f923a5acbbd9",
+    "url": "/static/media/soy_sauce.d47132d1.png"
+  },
+  {
+    "revision": "eb9ff440db7d555755b73932d774bec0",
+    "url": "/static/media/luckycat.eb9ff440.glb"
+  },
+  {
+    "revision": "d24dc50d7fd4d7f96ab14d6b1b75eac3",
+    "url": "/static/media/blue_fish.d24dc50d.png"
+  },
+  {
+    "revision": "df1e90bcc02238e5320112819aaa6fd4",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "daniellehoo.com/precache-manifest.c196ea60e2bcad08b112ab417ad0b95b.js"
+  "/precache-manifest.d61481ed9a30cdb6daacc211b028e0ec.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("daniellehoo.com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
