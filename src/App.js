11d6e7e4769752d2d367ccad400938cf83b82a12
Danielle Hoo <daniellem.hoo@gmail.com>
2020-04-26 13:37:17 -0400

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import Project from './components/project'
+import React, { useState, useEffect } from "react";
+import Project from "./components/project";
 import {
   Router,
   Switch,
@@ -7,72 +7,72 @@ import {
   Redirect,
   Link,
   useParams,
-  withRouter
-} from 'react-router-dom'
-import { createBrowserHistory } from 'history'
-import About from './components/about'
-import Landing from './components/landing'
-import Projects from './components/projects'
-import Model from './components/threejs'
-import './App.css'
+  withRouter,
+} from "react-router-dom";
+import { createBrowserHistory } from "history";
+import About from "./components/about";
+import Landing from "./components/landing";
+import Projects from "./components/projects";
+import Model from "./components/threejs";
+import "./App.css";
 
-const history = createBrowserHistory()
+const history = createBrowserHistory();
 
 const projects = [
   {
     id: 0,
-    title: 'Chinatown USA',
-    url: 'chinatown_usa',
-    img: 'https://live.staticflickr.com/65535/49593430952_712dc0cce1_b.jpg',
-    description: 'Chinatown, USA was inspired by The Chinese in America'
+    title: "Chinatown USA",
+    url: "chinatown_usa",
+    img: "https://live.staticflickr.com/65535/49593430952_712dc0cce1_b.jpg",
+    description: "Chinatown, USA was inspired by The Chinese in America",
   },
   {
     id: 1,
     title: "Our Children's Trust",
-    url: 'our_childrens_trust',
-    img: 'https://live.staticflickr.com/65535/49592718198_6760f8c00d_b.jpg',
+    url: "our_childrens_trust",
+    img: "https://live.staticflickr.com/65535/49592718198_6760f8c00d_b.jpg",
 
-    description: "our childreen's trustdfssdf "
+    description: "our childreen's trustdfssdf ",
   },
   {
     id: 2,
-    title: 'Nicaragua',
-    url: 'nicaragua',
-    img: 'https://live.staticflickr.com/65535/49593769191_4de582e8d0_b.jpg',
-    description: 'GIEI NICA '
+    title: "Nicaragua",
+    url: "nicaragua",
+    img: "https://live.staticflickr.com/65535/49593769191_4de582e8d0_b.jpg",
+    description: "GIEI NICA ",
   },
 
   {
     id: 3,
-    title: 'Amnesty International',
-    url: 'amnesty_international',
-    img: 'https://live.staticflickr.com/65535/49593280613_513f9fe243_b.jpg',
-    description: 'teear gassss '
-  }
-]
+    title: "Amnesty International",
+    url: "amnesty_international",
+    img: "https://live.staticflickr.com/65535/49593280613_513f9fe243_b.jpg",
+    description: "teear gassss ",
+  },
+];
 
-const ProjectContext = React.createContext(projects)
+const ProjectContext = React.createContext(projects);
 
-function App () {
+function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router history={history} basename={`${process.env.PUBLIC_URL}/`}>
         <Switch>
-          <Route exact path='/' component={Landing} />
+          <Route exact path="/" component={Landing} />
           <Route
-            path='/projects'
-            render={props => <Projects {...props} projectsData={projects} />}
+            path="/projects"
+            render={(props) => <Projects {...props} projectsData={projects} />}
           />
-          <Route path='/about' component={About} />
+          <Route path="/about" component={About} />
           <Route
-            exact path='/:url'
-            render={props => <Project {...props} projectsData={projects} />}
+            path={`${process.env.PUBLIC_URL}/:url`}
+            render={(props) => <Project {...props} projectsData={projects} />}
           />
         </Switch>
       </Router>
     </div>
-  )
+  );
 }
 
-export default withRouter(App)
+export default withRouter(App);
 // <Project data={projects} />
